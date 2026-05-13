57e3907f64e4ebe9adeb1e36c2d5e18050d326cc
LoiTang <heobovidai@gmail.com>
2026-05-13 10:46:39 +0700

    config to deploy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,41 @@
 import gulp from "gulp";
 import gulpSass from "gulp-sass";
-import dartSass from 'sass'
-const sass = gulpSass(dartSass)
+import dartSass from "sass";
 
+const sass = gulpSass(dartSass);
+
+// 1. Biên dịch Sass -> CSS
 export const buildStyle = () => {
-  return gulp.src("src/sass/**/*.scss").pipe(sass().on('err', sass.logError)).pipe(gulp.dest('dist/css'))
-}
+  return gulp
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"));
+};
+
+// 2. Copy toàn bộ các file HTML từ src ra dist
+export const copyHtml = () => {
+  return gulp.src("src/*.html").pipe(gulp.dest("dist"));
+};
+
+// 3. Copy các thư mục Assets (fonts, img, js) từ src ra dist
+export const copyAssets = () => {
+  return gulp
+    .src(["src/fonts/**/*", "src/img/**/*", "src/js/**/*"], { base: "src" })
+    .pipe(gulp.dest("dist"));
+};
+
+// 4. Task Watch (dùng khi code ở máy)
 export const watch = () => {
-  return gulp.watch("src/sass/**/*.scss",buildStyle);
-}
-export const build = buildStyle
-export default gulp.series(buildStyle,watch)
+  gulp.watch("src/sass/**/*.scss", buildStyle);
+  gulp.watch("src/*.html", copyHtml);
+  gulp.watch(["src/fonts/**/*", "src/img/**/*", "src/js/**/*"], copyAssets);
+};
+
+// 5. TASK BUILD CHÍNH (Netlify sẽ chạy cái này)
+export const build = gulp.series(
+  buildStyle,
+  gulp.parallel(copyHtml, copyAssets),
+);
+
+// 6. Task default
+export default gulp.series(build, watch);
