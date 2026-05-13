9e723d7a243355861854844329d3bb47585572b9
LoiTang <heobovidai@gmail.com>
2026-05-13 10:54:20 +0700

    config img

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,7 @@ import dartSass from "sass";
 
 const sass = gulpSass(dartSass);
 
-// 1. Biên dịch Sass -> CSS
+// 1. Task biên dịch Sass -> CSS vào dist/css
 export const buildStyle = () => {
   return gulp
     .src("src/sass/**/*.scss")
@@ -12,26 +12,29 @@ export const buildStyle = () => {
     .pipe(gulp.dest("dist/css"));
 };
 
-// 2. Copy toàn bộ các file HTML từ src ra dist
+// 2. Task copy file HTML từ src vào dist
 export const copyHtml = () => {
-  return gulp.src("src/*.html").pipe(gulp.dest("dist"));
+  return gulp
+    .src("src/*.html") // Tìm file html trong thư mục src
+    .pipe(gulp.dest("dist")); // Đẩy vào thư mục dist
 };
 
-// 3. Copy các thư mục Assets (fonts, img, js) từ src ra dist
+// 3. TASK COPY ẢNH VÀ CÁC THƯ MỤC KHÁC (img, js, fonts) VÀO DIST
+// Chìa khóa nằm ở { base: "src" } - nó giúp giữ nguyên cấu trúc thư mục
 export const copyAssets = () => {
   return gulp
     .src(["src/fonts/**/*", "src/img/**/*", "src/js/**/*"], { base: "src" })
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("dist")); // Đẩy tất cả vào dist, ví dụ src/img/iPhone/ -> dist/img/iPhone/
 };
 
 // 4. Task Watch (dùng khi code ở máy)
 export const watch = () => {
   gulp.watch("src/sass/**/*.scss", buildStyle);
   gulp.watch("src/*.html", copyHtml);
-  gulp.watch(["src/fonts/**/*", "src/img/**/*", "src/js/**/*"], copyAssets);
+  gulp.watch(["src/fonts/**/*", "src/img/**/*", "src/js/**/*"], copyAssets); // Watch cả thay đổi ảnh/js
 };
 
-// 5. TASK BUILD CHÍNH (Netlify sẽ chạy cái này)
+// 5. TASK BUILD CHÍNH (Đảm bảo chạy cả build CSS và copy assets)
 export const build = gulp.series(
   buildStyle,
   gulp.parallel(copyHtml, copyAssets),
