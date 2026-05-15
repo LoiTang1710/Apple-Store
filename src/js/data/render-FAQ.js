2639632de2cfee48bedd1efcdfa79460ea2523b6
LoiTang <heobovidai@gmail.com>
2026-05-15 20:09:01 +0700

    feat: Implement Schema Markup

--- src/js/data/render-FAQ.js
+++ src/js/data/render-FAQ.js
@@ -11,4 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("AEO-container")
     .insertAdjacentHTML("beforeend", FAQContainer);
+
+    // Tạo schema markup động
+    const schemaData = {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      "mainEntity": FAQs.map((item) => ({
+        "@type": "Question",
+        "name": item.question,
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": item.answer
+        }
+      }))
+    }
+    // console.log(schemaData)
+    const scriptTag = document.querySelector('script[type="application/ld+json"]')
+    if(scriptTag){
+      scriptTag.textContent = JSON.stringify(schemaData)
+      // console.log("scriptTag: ", scriptTag.textContent)
+    }
 });
